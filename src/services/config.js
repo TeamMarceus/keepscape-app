efb4983c0efa41da4bf639ae32ad00579d66652e
Stephine Sinoy <stephine.sinoy@cit.edu>
2023-10-18 10:36:39 +0800

    Update url

--- src/services/config.js
+++ src/services/config.js
@@ -3,7 +3,7 @@ import { isLocal } from '../utils/destinations';
 let apiUrl = 'https://keepscape-api20231018021452.azurewebsites.net';
 
 if (isLocal) {
-  apiUrl = 'https://localhost:7104';
+  apiUrl = 'https://keepscape-api20231018021452.azurewebsites.net';
 }
 
 const config = {
