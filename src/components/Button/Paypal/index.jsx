0b2e8ec1c20c7a84258166af844f3249a25d42b4
Stephine Sinoy <stephine.sinoy@cit.edu>
2023-11-02 01:23:59 +0800

    Update paypal button

--- src/components/Button/Paypal/index.jsx
+++ src/components/Button/Paypal/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 
 import PropTypes from 'prop-types';
+import { toast } from 'sonner';
 
 function PayPalButton({ total }) {
   const paypalRef = useRef();
@@ -10,18 +11,28 @@ function PayPalButton({ total }) {
       createOrder: (data, actions) => actions.order.create({
           purchase_units: [{
             amount: {
-              currency_code: 'USD',
+              currency_code: 'PHP',
               value: total,
             },
           }],
         }),
       onApprove: (data, actions) => actions.order.capture().then((details) => {
-          alert(`Transaction completed by ${  details.payer.name.given_name}`);
+          toast.success(`Transaction completed by ${details.payer.name.given_name}`, {
+            style: {
+              backgroundColor: '#1ab394',
+              color: '#fff',
+            },
+          });
           // TODO: Send transaction data to your server for further processing.
         }),
       onError: (err) => {
         console.error(err);
-        alert('An error occurred during the transaction.');
+        toast.error('An error occurred during the transaction.', {
+          style: {
+            backgroundColor: '#ed5565',
+            color: '#fff',
+          },
+        });
       }
     }).render(paypalRef.current);
   }, [total]);
